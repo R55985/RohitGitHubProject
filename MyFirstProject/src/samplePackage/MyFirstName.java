8d5d3e6f6302a1253ccbbeec3026d679f8ab11f8
R55985 <110164178+R55985@users.noreply.github.com>
2022-07-30 11:53:30 +0530

    MyPosition
    He check it

--- MyFirstProject/src/samplePackage/MyFirstName.java
+++ MyFirstProject/src/samplePackage/MyFirstName.java
@@ -5,6 +5,7 @@ public class MyFirstName {
 	public static void main(String[] args) {
 		System.out.println("My First Name is Rohit");
 		System.out.println("My Last Name is Suryawanshi");
+		System.out.println("I am Quality Analyst");
 	}
 
 }
